c077035d9d3918c7eb6b15e5e94506ac71376bb6
hero1 <hero1@TORNADO>
2025-12-08 18:07:37 +0300

    Add project files.

---
+++ Beginner/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int age = 32;
+age = 33;
+long age1 = 9000L;
+Console.WriteLine(age);
+age++;
+Console.WriteLine(age);
+Console.WriteLine(age1);
+double test = 55.01D;
+float test1 = 5.00001F;
+Console.WriteLine(test);
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(long.MaxValue);
+Console.WriteLine(double.MaxValue);
+const decimal test2 = 14.99999999999999M;
+Console.WriteLine(decimal.MaxValue);
+Console.WriteLine(test2);
+string name = "Hello";
+Console.WriteLine(name);
+char letter = 'A';
+Console.WriteLine(letter);
+bool test5 = true;
+Console.WriteLine(test5);
+Console.ReadLine();
