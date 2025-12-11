f86dc16f4b7f2c190bd6db4385cc0028a0fdcf6e
TornadoLaptop <tornadoheroes337@gmail.com>
2025-12-11 20:15:07 +0300

    progress of learning, if function and else and elif

--- Beginner/Program.cs
+++ Beginner/Program.cs
@@ -1,27 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//testing 123 
-int age = 32;
-age = 33;
-long age1 = 9000L;
-Console.WriteLine(age);
-age++;
-Console.WriteLine(age);
-Console.WriteLine(age1);
-double test = 55.01D;
-float test1 = 5.00001F;
-Console.WriteLine(test);
-Console.WriteLine(int.MaxValue);
-Console.WriteLine(long.MaxValue);
-Console.WriteLine(double.MaxValue);
-const decimal test2 = 14.99999999999999M;
-Console.WriteLine(decimal.MaxValue);
-Console.WriteLine(test2);
-string name = "Hello";
-Console.WriteLine(name);
-char letter = 'A';
-Console.WriteLine(letter);
-bool test5 = true;
-Console.WriteLine(test5);
-Console.ReadLine();
-Console.WriteLine("Hello World!");
-Console.ReadLine();
+using System.ComponentModel.Design;
+
+Console.Write("Whats your name? ");
+string name = Console.ReadLine();
+Console.Write("How tall are you? ");
+int height = Convert.ToInt32(Console.ReadLine());
+Console.Write("How old are you? ");
+int age = Convert.ToInt32(Console.ReadLine());
+if (age <30 || height >170)
+{
+    Console.WriteLine("Eligible for discount");
+}
+else 
+{
+    Console.WriteLine("No discount");
+}
